769a9942b366cbe67aa4c6b5343ec371984463bd
senthilmnathan <senthil.nathanm@yahoo.com>
2022-03-19 18:24:32 +0000

    Senthil Commit's Sat Mar 19 18:24:32 UTC 2022

---
+++ security/installation/master_firewall.sh
@@ -0,0 +1,9 @@
+dnf install -y firewalld
+systemctl enable firewalld && systemctl start firewalld
+firewall-cmd --permanent --add-port=6443/tcp
+firewall-cmd --permanent --add-port=2379-2380/tcp
+firewall-cmd --permanent --add-port=10250/tcp
+firewall-cmd --permanent --add-port=10251/tcp
+firewall-cmd --permanent --add-port=10252/tcp
+firewall-cmd --permanent --add-port=10255/tcp
+systemctl restart firewalld
